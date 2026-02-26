a2a72300f62a026b9f39c41d915b06fafae3bd97
pana-cc <panayot.cankov@gmail.com>
2026-02-26 15:29:32 +0200

    chore: XuiSDK taller window title

--- Xui/Apps/XuiSDK/MainWindow.cs
+++ Xui/Apps/XuiSDK/MainWindow.cs
@@ -1,4 +1,6 @@
+using System.Runtime.InteropServices;
 using Xui.Core.Abstract;
+using Xui.Core.Abstract.Events;
 using Xui.Core.Math2D;
 using static Xui.Core.Abstract.IWindow.IDesktopStyle;
 
@@ -6,6 +8,8 @@ namespace Xui.Apps.XuiSDK;
 
 public class MainWindow : Xui.Core.Abstract.Window, IWindow.IDesktopStyle
 {
+    private NFloat HeaderHeight = 48;
+    
     WindowBackdrop IWindow.IDesktopStyle.Backdrop => WindowBackdrop.Mica;
     Size? IWindow.IDesktopStyle.StartupSize => new Size(900, 600);
     WindowClientArea IWindow.IDesktopStyle.ClientArea => WindowClientArea.Extended;
@@ -13,5 +17,11 @@ public class MainWindow : Xui.Core.Abstract.Window, IWindow.IDesktopStyle
     public MainWindow(IServiceProvider context) : base(context)
     {
         Content = new NavigationShell();
+    }
+
+    public override void WindowHitTest(ref WindowHitTestEventRef evRef)
+    {
+        if (evRef.Point.Y < HeaderHeight)
+            evRef.Area = WindowHitTestEventRef.WindowArea.Title;
     }
 }
